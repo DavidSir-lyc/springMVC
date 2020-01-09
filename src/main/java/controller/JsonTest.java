5b6a9e479664a2bb2943ab06f966335675a6a1c2
DavidSir <1135130110@qq.com>
2020-01-09 14:38:40 +0800

    springMVC学习 --- 接受请求参数 学习 1

--- src/main/java/controller/JsonTest.java
+++ src/main/java/controller/JsonTest.java
@@ -7,10 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.alibaba.fastjson.JSON;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
 
 @Controller
 class JsonTest {
@@ -38,6 +36,7 @@ class JsonTest {
 
     @RequestMapping("/jsonTest02")
     @ResponseBody
+    @CrossOrigin(origins = "*", maxAge = 3600)
     public List<Map<String,Object>> doMapJsonString02(){
         List<Map<String,Object>> list = new ArrayList<>();
         Map<String, Object> map = new LinkedHashMap<>();
@@ -51,5 +50,37 @@ class JsonTest {
         // System.out.println(JSON.toJSONString(list));
         return list;
         //系统底层会基于返回值查找对应的转换,将对象转换为指定格式的字符串
+    }
+
+    /**
+     * 通过形参接收请求参数，形参名称与请求参数名称完全相同
+     * 改参数名就要改形参名，H5传参名错误，java接受不到抛500，不完美
+     */
+    @RequestMapping("/testParams01")
+    @ResponseBody
+    @CrossOrigin(origins = "*", maxAge = 3600)
+    public Map<String,Object> testParamsFun01(String name, int age){
+        HashMap<String, Object> map = new LinkedHashMap<>();
+        map.put("returnName", name);
+        map.put("returnAge", age);
+        // return JSON.parseObject(String.valueOf(map));
+        // System.out.println(JSON.toJSON(map));
+        // System.out.println(JSON.toJSONString(map));
+        return (Map<String, Object>) JSON.toJSON(map);
+    }
+
+    /**
+     * 通过HttpServletRequest接收请求参数
+     */
+    @RequestMapping("/testParams02")
+    @ResponseBody
+    @CrossOrigin(origins = "*", maxAge = 3600)
+    public Map<String,Object> testParamsFun02(HttpServletRequest request){
+        HashMap<String, Object> map = new LinkedHashMap<>();
+        String name = request.getParameter("name");
+        String age = request.getParameter("age");
+        map.put("returnName", name);
+        map.put("returnAge", age);
+        return (Map<String, Object>) JSON.toJSON(map);
     }
 }
