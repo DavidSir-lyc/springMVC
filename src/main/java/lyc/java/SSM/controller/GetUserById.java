ff4914d82c2b3206719331ab724880ea09787b07
DavidSir <1135130110@qq.com>
2020-05-18 14:35:06 +0800

    springMVC学习 --- 加一些注释

--- src/main/java/lyc/java/SSM/controller/GetUserById.java
+++ src/main/java/lyc/java/SSM/controller/GetUserById.java
@@ -19,6 +19,25 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/*  常用注解
+    @Controller：标识这个类是一个控制器
+    @RequestMapping：给控制器方法绑定一个uri
+    @ResponseBody：将java对象转成json，并且发送给客户端
+    @RequestBody：将客户端请求过来的json转成java对象
+    @RequestParam：当表单参数和方法形参名字不一致时，做一个名字映射
+    @PathVarible：用于获取uri中的参数,比如user/1中1的值
+
+    Rest风格的新api
+    @RestController相当于@Controller+ @ResponseBody
+    @GetMapping@DeleteMapping@PostMapping@PutMapping
+
+    其他注解
+    @SessionAttribute：声明将什么模型数据存入session
+    @CookieValue：获取cookie值
+    @ModelAttribute：将方法返回值存入model中
+    @HeaderValue：获取请求头中的值
+*/
+
 @Controller
 public class GetUserById {
     @GetMapping("/getUserById")
